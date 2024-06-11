ea598df841a5533e65cb837480170483f7e04602
wooseong-choi <ghkdgo8686@gmail.com>
2024-06-11 12:28:03 +0900

    커밋 전 푸시

--- vm/anon.c
+++ vm/anon.c
@@ -21,7 +21,8 @@ static const struct page_operations anon_ops = {
 void
 vm_anon_init (void) {
 	/* TODO: Set up the swap_disk. */
-	swap_disk = NULL;
+	swap_disk = disk_get(1,1);
+	
 }
 
 /* Initialize the file mapping */
@@ -43,6 +44,14 @@ anon_swap_in (struct page *page, void *kva) {
 static bool
 anon_swap_out (struct page *page) {
 	struct anon_page *anon_page = &page->anon;
+	// 디스크 스왑 아웃 
+	// 물리 메모리 공간은 유지해야 한다.
+	// 즉 매핑된 물리 메모리에 가상메모리에 있는 데이터를 써줘야 한다? 어떻게?
+	// 특정한 물리 메모리에 매핑되어 있는, 모든 데이터와 이전 주소를 가진 테이블
+	// 스왑 테이블. 1이 둘중 하나인것 같은데 잘 모르겠음
+	// 1-. 초기화시 init에서 이미 초기화 된 스왑 테이블을 anon page에 연결
+	// 1=. 초기화시 init에서 스왑 디스크를 초기화하고 그걸 anon page에 연결
+
 }
 
 /* Destroy the anonymous page. PAGE will be freed by the caller. */
